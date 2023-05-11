56749022228da721196be730aaaf96b6b3a9688d
yockii <xuyuqi@gmail.com>
2023-05-11 19:07:57 +0800

    增加项目角色类型

--- internal/module/uc/model/Role.go
+++ internal/module/uc/model/Role.go
@@ -6,7 +6,7 @@ type Role struct {
 	ID         uint64 `json:"id,omitempty,string" gorm:"primaryKey;autoIncrement:false"`
 	RoleName   string `json:"roleName,omitempty" gorm:"size:50;comment:角色名称"`
 	RoleDesc   string `json:"roleDesc,omitempty" gorm:"size:200;comment:角色描述"`
-	RoleType   int    `json:"roleType,omitempty" gorm:"comment:角色类型 1-普通角色 -1-超级管理员角色"`
+	RoleType   int    `json:"roleType,omitempty" gorm:"comment:角色类型 1-普通角色 2-项目角色 -1-超级管理员角色"`
 	CreateTime int64  `json:"createTime" gorm:"autoCreateTime:milli"`
 	UpdateTime int64  `json:"updateTime" gorm:"autoUpdateTime:milli"`
 }
