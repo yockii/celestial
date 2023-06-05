6b164bb0e565e42df5a23b50ef4d82fb92ff433d
yockii <xuyuqi@gmail.com>
2023-06-05 15:59:39 +0800

    增加登出功能

---
+++ page/source/src/components/user/UserDropdown.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useUserStore } from "@/store/user"
+
+defineProps<{
+  name: string
+}>()
+const options = ref([
+  {
+    label: "退出登录",
+    key: "logout"
+  }
+])
+const router = useRouter()
+const handleSelect = (key: string) => {
+  if (key === "logout") {
+    useUserStore().logout()
+    router.push("/login")
+  }
+}
+</script>
+
+<template>
+  <n-dropdown trigger="click" :options="options" @select="handleSelect">
+    <n-text>{{ name }}</n-text>
+  </n-dropdown>
+</template>
